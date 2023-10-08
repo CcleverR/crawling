aef622d70278cab39483c6258b20597521ff4bb9
CcleverR <CcleverR>
2023-10-08 19:28:16 +0900

    가공된정보

--- 과제/hos_crol.py
+++ 과제/hos_crol.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 
 
 class Doctor:
-
+    name = None
+    major = None
+    specialization = None
     def __init__(self, name, major, Specialization) -> None:
-        self.name = None
+        self.name = name
+        self.major=major
+        self.specialization=Specialization
+
+
+
 
 
 
@@ -18,17 +25,28 @@ if __name__== "__main__":
         html=response.text
         soup=BeautifulSoup(html,'lxml')
         ul=soup.select_one("#doctorForm > div.contents > ul")
-        profiles=ul.select("li > a > p.dept > span")
+        majors=ul.select("li > a > p.dept > span")
         info_names=ul.select("li > a > p.name")
         info_Specializations=ul.select(" li > a > p.text01")  
         #doctorForm > div.contents > ul > li:nth-child(1) > a > p.text01
         
-        for i in range(len(profiles)):
-            print(i+1,profiles[i].get_text())
-        for i in range(len(info_names)):
-            print(i+1,info_names[i].get_text())
-        for i in range(len(info_Specializations)):
-            print(i+1,info_Specializations[i].get_text())
+        doctors = []        
+        for i in range(len(majors)):
+            major = majors[i].get_text()
+            name = info_names[i].get_text()
+            specialization = info_Specializations[i].get_text()
+            d = Doctor(name,major,specialization)
+            doctors.append(d)
+
+        for i in range(len(doctors)):
+            print(i+1, " doctor info")
+            print("\tname:", doctors[i].name)
+            print("\tmajor:", doctors[i].major)
+            print("\tspecialization:", doctors[i].specialization)
+
+
+
+
 
 
         
